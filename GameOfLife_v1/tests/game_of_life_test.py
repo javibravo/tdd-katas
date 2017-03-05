2d4b1b215fb27e0735280462d87bdd29bbe3a00f
Javier Bravo <javibravo85@gmail.com>
2017-03-05 18:38:56 +0000

    Alive cell with more than 3 live neighbours dies

--- GameOfLife_v1/tests/game_of_life_test.py
+++ GameOfLife_v1/tests/game_of_life_test.py
@@ -1,7 +1,7 @@
 from src.cell import Cell
 
 
-def test_alive_cell_with_fewer_than_1_neighbours_dies():
+def test_alive_cell_with_fewer_fewer_2_neighbours_dies():
     cell = Cell(Cell.ALIVE)
     live_neighbours = 1
 
@@ -10,11 +10,12 @@ def test_alive_cell_with_fewer_than_1_neighbours_dies():
     assert cell.status == Cell.DIED
 
 
-def test_alive_cell_with_fewer_than_0_neighbours_dies():
+def test_alive_cell_with_more_than_3_neighbours_dies():
     cell = Cell(Cell.ALIVE)
-    live_neighbours = 0
+    live_neighbours = 4
 
     cell.next_stage(live_neighbours)
 
     assert cell.status == Cell.DIED
+
 
