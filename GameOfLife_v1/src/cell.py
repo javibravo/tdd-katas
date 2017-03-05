2ae1c043311f59e61f338f135b1a7c915e461365
Javier Bravo <javibravo85@gmail.com>
2017-03-05 19:05:40 +0000

    Evolve from dead cell

--- GameOfLife_v1/src/cell.py
+++ GameOfLife_v1/src/cell.py
@@ -1,10 +1,15 @@
 class Cell:
 
     ALIVE = 1
-    DIED = 0
+    DEAD = 0
 
     def __init__(self, status=ALIVE):
         self.status = status
 
     def next_stage(self, live_neighbours):
-        self.status = Cell.DIED
+        if self.status == Cell.ALIVE and (live_neighbours == 2 or live_neighbours == 3):
+            self.status = Cell.ALIVE
+        elif self.status == Cell.DEAD and live_neighbours == 3:
+            self.status = Cell.ALIVE
+        else:
+            self.status = Cell.DEAD
--- GameOfLife_v1/tests/game_of_life_test.py
+++ GameOfLife_v1/tests/game_of_life_test.py
@@ -7,7 +7,7 @@ def test_alive_cell_with_fewer_fewer_2_neighbours_dies():
 
     cell.next_stage(live_neighbours)
 
-    assert cell.status == Cell.DIED
+    assert cell.status == Cell.DEAD
 
 
 def test_alive_cell_with_more_than_3_neighbours_dies():
@@ -16,6 +16,42 @@ def test_alive_cell_with_more_than_3_neighbours_dies():
 
     cell.next_stage(live_neighbours)
 
-    assert cell.status == Cell.DIED
+    assert cell.status == Cell.DEAD
+
+
+def test_alive_cell_with_2_neighbours_keep_alive():
+    cell = Cell(Cell.ALIVE)
+    live_neighbours = 2
+
+    cell.next_stage(live_neighbours)
+
+    assert cell.status == Cell.ALIVE
+
+
+def test_alive_cell_with_3_neighbours_keep_alive():
+    cell = Cell(Cell.ALIVE)
+    live_neighbours = 3
+
+    cell.next_stage(live_neighbours)
+
+    assert cell.status == Cell.ALIVE
+
+
+def test_dead_cell_with_3_neighbours_change_to_live():
+    cell = Cell(Cell.DEAD)
+    live_neighbours = 3
+
+    cell.next_stage(live_neighbours)
+
+    assert cell.status == Cell.ALIVE
+
+
+def test_dead_cell_with_2_neighbours_keep_dead():
+    cell = Cell(Cell.DEAD)
+    live_neighbours = 2
+
+    cell.next_stage(live_neighbours)
+
+    assert cell.status == Cell.DEAD
 
 
